51aa5b207d0693c18fc86c81cb95e5a236065bf0
Ramkrishna Birla <rmkbirla@gmail.com>
2024-08-11 23:20:57 +0530

    connection

--- src/database.js
+++ src/database.js
@@ -1,10 +1,23 @@
 const mysql = require('mysql2');
+const { URL } = require('url');
+
+// Connection string
+const connectionString = 'mysql://root:vAPwyjyVJNMRBeZgpFZjpyagSCqyRQza@mysql.railway.internal:3306/railway';
+
+// Parse the connection string
+const url = new URL(connectionString);
+const [user, password] = url.username ? [url.username, url.password] : [null, null];
 
 const pool = mysql.createPool({
-    host: 'banner.railway.internal',
-    user: 'root', 
-    password: 'vAPwyjyVJNMRBeZgpFZjpyagSCqyRQza',
-    database: 'railway'
+    host: url.hostname,
+    user: user,
+    password: password,
+    database: url.pathname.substring(1),
+    port: url.port || 3306,
+    // If you're using a newer version of Node.js and `mysql2`, you might want to add the following option for connection pooling
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
 const promisePool = pool.promise();
